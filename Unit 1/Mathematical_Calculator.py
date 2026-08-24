9f0635aa9927ff1a094d6b52b6701438ae2d57d7
Donishree <juug25btech23532@jainuniversity.ac.in>
2026-08-24 19:54:33 +0530

    Add Unit 1 Python lab works

---
+++ Unit 1/Mathematical_Calculator.py
@@ -0,0 +1,13 @@
+import math
+
+num = float(input("Enter a number: "))
+
+print("\n------ RESULTS ------")
+print("Square        :", num ** 2)
+print("Cube          :", num ** 3)
+print("Square Root   :", math.sqrt(num))
+print("Ceiling Value :", math.ceil(num))
+print("Floor Value   :", math.floor(num))
+print("Absolute Value:", abs(num))
+print("Type          :", type(num))
+print("Memory Address:", id(num))
