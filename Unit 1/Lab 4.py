7e2f8fcb77d426f06789993c3907836dc9ed4826
Donishree <juug25btech23532@jainuniversity.ac.in>
2026-08-24 20:54:58 +0530

    Add Lab 4 script for number calculations

---
+++ Unit 1/Lab 4.py
@@ -0,0 +1,14 @@
+import math
+
+# Taking input
+num = float(input("Enter a floating point number: "))
+
+# Displaying results
+print("Square: ", num ** 2)
+print("Cube: ", num ** 3)
+print("Square Root: ", math.sqrt(num))
+print("Ceiling Value: ", math.ceil(num))
+print("Floor Value: ", math.floor(num))
+print("Absolute Value: ", abs(num))
+print("Type of the Variable: ", type(num))
+print("Memory Address (ID): ", id(num))
